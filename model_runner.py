cbc3b74b1f2fb549611e57c35f0366f1c0e19e6f
HanChangHun <ehwjs1914@ajou.ac.kr>
2022-09-29 14:19:10 +0900

    seg & sram test for object detection

---
+++ model_runner.py
@@ -0,0 +1,232 @@
+from pprint import pprint
+import time
+import collections
+
+import numpy as np
+from PIL import Image
+
+import tflite_runtime.interpreter as tflite
+
+from pycoral.adapters import common
+from pycoral.adapters import classify
+from pycoral.adapters.detect import BBox
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+
+labels = read_label_file(
+    "models/object_detection/data/model_result/road_sign_labels.txt"
+)
+input_file = "models/object_detection/data/images/train/2019-04-16-095702.jpg"
+
+
+class ModelRunner:
+    def __init__(self, model_paths):
+        self.model_paths = model_paths
+
+        self.interpreters: list[tflite.Interpreter] = []
+        self.make_interpreters()
+
+        self.intermediate = dict()
+        self.cur_idx = 0
+        self.cur_det_scale = None
+
+        output_details = self.interpreters[-1].get_output_details()[0]
+        self.scale, self.zero_point = output_details["quantization"]
+
+        self.size = common.input_size(self.interpreters[0])
+        _dtype = self.interpreters[0].get_input_details()[0]["dtype"]
+
+        self.image = Image.open(input_file)
+        self.preloaded_img1 = self.image.convert("RGB").resize(
+            self.size, Image.ANTIALIAS
+        )
+        self.preloaded_img2 = np.asarray(
+            self.preloaded_img1,
+            dtype=_dtype,
+        )[np.newaxis, :]
+
+    def make_interpreters(self):
+        for model_path in self.model_paths:
+            self.interpreters.append(make_interpreter(str(model_path)))
+
+    def allocate_tensors_all_interpreter(self):
+        for interpreter in self.interpreters:
+            interpreter.allocate_tensors()
+
+    def invoke_all(self, task=None):
+        self.cur_idx = 0
+        for _ in range(len(self.interpreters)):
+            self.invoke_and_next(task=task)
+
+    def invoke_and_next(self, task=None):
+        assert self.cur_idx < len(self.interpreters)
+
+        self.invoke_idx(self.cur_idx, task=task)
+
+        if self.cur_idx < len(self.interpreters) - 1:
+            self.cur_idx += 1
+            return 0
+
+        elif self.cur_idx == len(self.interpreters) - 1:
+            self.cur_idx = 0
+            return 1
+
+        else:
+            raise Exception("wired index...")
+
+    def invoke_idx(self, idx, task=None, profile=False):
+        interpreter = self.interpreters[idx]
+
+        h2d_dur = self.set_input(idx, task=task, profile=profile)
+        exec_dur = self.invoke(interpreter, profile=profile)
+        d2h_dur = self.update_intermediate(interpreter, profile=profile)
+
+        if profile:
+            return [h2d_dur, exec_dur, d2h_dur]
+
+    def set_first_input_orginal(self, input_file):
+        # Deprecated
+        assert self.intermediate == None
+        common.set_input(self.interpreters[0], self.preloaded_img1)
+
+    def set_input(self, idx, task=None, profile=False):
+        if idx == 0:
+            duration = self.set_first_input(task=task, profile=profile)
+        else:
+            duration = self.set_general_input(self.interpreters[idx], profile=profile)
+
+        if profile:
+            return duration
+
+    def set_first_input(self, task=None, profile=False):
+        if profile:
+            st = time.perf_counter()
+
+        if task == None:
+            input_details = self.interpreters[0].get_input_details()
+            for input_detail in input_details:
+                if input_detail["name"] == "images":
+                    tensor_index = input_detail["index"]
+                    self.interpreters[0].set_tensor(tensor_index, self.preloaded_img2)
+
+        elif task == "detection":
+            self.set_sized_input(
+                self.interpreters[0],
+                self.image.size,
+                lambda size: self.image.resize(size, Image.ANTIALIAS),
+            )
+
+        if profile:
+            duration = (time.perf_counter() - st) * 1000
+            return duration
+
+    def set_sized_input(self, interpreter, size, resize):
+        _, height, width, _ = interpreter.get_input_details()[0]["shape"]
+        w, h = size
+        scale = min(width / w, height / h)
+        w, h = int(w * scale), int(h * scale)
+        tensor = interpreter.tensor(interpreter.get_input_details()[0]["index"])()[0]
+        tensor.fill(0)
+        _, _, channel = tensor.shape
+        result = resize((w, h))
+        tensor[:h, :w] = np.reshape(result, (h, w, channel))
+
+        self.cur_det_scale = (scale, scale)
+
+    def set_general_input(self, interpreter, profile=False):
+        assert self.intermediate != None
+        if profile:
+            st = time.perf_counter()
+
+        input_details = interpreter.get_input_details()
+        # print("---")
+        # pprint(input_details)
+        print("---")
+        pprint(list(self.intermediate.keys()))
+        print("---")
+        for input_detail in input_details:
+            for k, v in self.intermediate.items():
+                if input_detail["name"] == k:
+                    interpreter.set_tensor(input_detail["index"], v)
+        if profile:
+            duration = (time.perf_counter() - st) * 1000
+            return duration
+
+    def invoke(self, interpreter, profile=False):
+        if profile:
+            st = time.perf_counter()
+
+        interpreter.invoke()
+
+        if profile:
+            duration = (time.perf_counter() - st) * 1000
+            return duration
+
+    def update_intermediate(self, interpreter, profile=False):
+        if profile:
+            st = time.perf_counter()
+
+        for o in interpreter.get_output_details():
+            self.intermediate[o["name"]] = interpreter.get_tensor(o["index"])
+
+        if profile:
+            duration = (time.perf_counter() - st) * 1000
+            return duration
+
+    def get_result(self, top_n=1, detect=False, image_scale=(1.0, 1.0)):
+        out = list(self.intermediate.items())
+        # assert len(out) == 1
+
+        _, values = out[0]
+        scores = self.scale * (values[0].astype(np.int64) - self.zero_point)
+        if detect == False:
+            classes = classify.get_classes_from_scores(scores, top_n, 0.0)
+            result = {labels.get(c.id, c.id): c.score for c in classes}
+        else:
+            result = self.get_objects(image_scale=image_scale)
+
+        self.intermediate = dict()
+
+        return result
+
+    def get_objects(self, score_threshold=-float("inf"), image_scale=(1.0, 1.0)):
+        Object = collections.namedtuple("Object", ["id", "score", "bbox"])
+
+        def make(i):
+            ymin, xmin, ymax, xmax = boxes[i]
+            return Object(
+                id=int(class_ids[i]),
+                score=float(scores[i]),
+                bbox=BBox(xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax)
+                .scale(sx, sy)
+                .map(int),
+            )
+
+        interpreter = self.interpreters[-1]
+        signature_list = interpreter._get_full_signature_list()
+        # pylint: enable=protected-access
+        if signature_list:
+            if len(signature_list) > 1:
+                raise ValueError("Only support model with one signature.")
+            signature = signature_list[next(iter(signature_list))]
+            count = int(interpreter.tensor(signature["outputs"]["output_0"])()[0])
+            scores = interpreter.tensor(signature["outputs"]["output_1"])()[0]
+            class_ids = interpreter.tensor(signature["outputs"]["output_2"])()[0]
+            boxes = interpreter.tensor(signature["outputs"]["output_3"])()[0]
+        elif common.output_tensor(interpreter, 3).size == 1:
+            boxes = common.output_tensor(interpreter, 0)[0]
+            class_ids = common.output_tensor(interpreter, 1)[0]
+            scores = common.output_tensor(interpreter, 2)[0]
+            count = int(common.output_tensor(interpreter, 3)[0])
+        else:
+            scores = common.output_tensor(interpreter, 0)[0]
+            boxes = common.output_tensor(interpreter, 1)[0]
+            count = (int)(common.output_tensor(interpreter, 2)[0])
+            class_ids = common.output_tensor(interpreter, 3)[0]
+
+        width, height = self.size
+        image_scale_x, image_scale_y = self.cur_det_scale
+        sx, sy = width / image_scale_x, height / image_scale_y
+        self.cur_det_scale = None
+
+        return [make(i) for i in range(count) if scores[i] >= score_threshold]
