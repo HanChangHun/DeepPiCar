6f162f1d14263efaa995cd77dc5b78c7030dfe91
HanChangHun <ehwjs1914@ajou.ac.kr>
2022-11-07 23:56:45 +0900

    add e2e latency

--- experiments/obj_det_sram/run_full_sram_det.py
+++ experiments/obj_det_sram/run_full_sram_det.py
@@ -14,8 +14,25 @@ from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.dataset import read_label_file
 
 
+def e2e_inference(interpreter, image_path):
+    image = cv2.imread(image_path)
+    image_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img_pil = Image.fromarray(image_RGB)
+    _, scale = common.set_resized_input(
+        interpreter,
+        img_pil.size,
+        lambda size: img_pil.resize(size, Image.ANTIALIAS),
+    )
+
+    interpreter.invoke()
+    objects = detect.get_objects(interpreter, score_threshold=0.3, image_scale=scale)
+
+
 if __name__ == "__main__":
     model_path = "experiments/obj_det_sram/models/full/efficientdet-lite_edgetpu.tflite"
+
+    image_path = "data/object_detection/test/frame_000004.JPEG"
+
     interpreter = make_interpreter(model_path)
     interpreter.allocate_tensors()
     interpreter.invoke()
@@ -26,4 +43,12 @@ if __name__ == "__main__":
         interpreter.invoke()
         time_spans.append((time.perf_counter() - st) * 1000)
 
-    print("Average time: %f ms" % (sum(time_spans) / len(time_spans)))
+    print("Average inference time: %f ms" % (sum(time_spans) / len(time_spans)))
+
+    time_spans = []
+    for _ in range(100):
+        st = time.perf_counter()
+        e2e_inference(interpreter, image_path)
+        time_spans.append((time.perf_counter() - st) * 1000)
+
+    print("Average e2e time: %f ms" % (sum(time_spans) / len(time_spans)))
